d7a2ca2c302c376aca94ef7d3024393b30b7025d
Lucineia2024 <lucineiamcaldeira@outlook.com>
2024-05-10 06:13:33 -0300

    Crição do código em JavaScript

---
+++ codigo-fonte/telaLogin/login.js
@@ -0,0 +1,14 @@
+
+
+    function logar(){
+        var email = document.getElementById("email").value;
+        var senha = document.getElementById("senha").value;
+
+        if(email == "teste@outlook.com" && senha == "teste"){
+            alert("Sucesso");
+        }else{
+            alert("Usuário ou senha incorretos");
+        }
+    }
+
+
