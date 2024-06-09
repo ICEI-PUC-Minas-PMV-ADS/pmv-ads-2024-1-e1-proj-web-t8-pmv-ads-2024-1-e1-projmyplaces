8ef9a3331acbe72aaaccfff91523d3cbcd32854a
Lucineia2024 <lucineiamcaldeira@outlook.com>
2024-06-09 07:55:51 -0300

    Reorganizando pastas e códigos

--- codigo-fonte/telaLogin/login.js
+++ codigo-fonte/telaLogin/login.js
@@ -8,7 +8,7 @@
         let msgErro = document.querySelector('#msgErro')
 
         /*let lista = []*/
-        let validUsuario = {
+        let validUsuario ={
             email: '',
             password: ''            
         }
@@ -16,7 +16,7 @@
         let lista = JSON.parse(localStorage.getItem('listUsuario'))
 
         lista.forEach((item) => {
-            if(email.value == item.emailUser && password.value == item.senhaUser){
+            if(email.value === item.emailUser && password.value === item.senhaUser){
                 validUsuario = {
                     email: item.emailUser,
                     password: item.senhaUser                
@@ -25,18 +25,27 @@
         })
         listausuario = JSON.stringify(validUsuario)
 
-        if(email.value === validUsuario.email && password.value === validUsuario.password){
-            alert('funcionou')
+        if(email.value === '' && password.value === ''){
 
-        } else
-        {
-            labelEmail.setAttribute('style', 'color: red')
-            email.setAttribute('style', 'border-color: red')
-            labelPassword.setAttribute('style', 'color: red')
-            password.setAttribute('style', 'border-color: red')
-            msgErro.setAttribute('style', 'display: block')
-            msgErro.innerHTML = 'Usuário ou senha incorretos'                          
+            msgCampoVazio.setAttribute('style', 'display: block')
+            msgCampoVazio.innerHTML = 'Digite usuário e senha'
             
+        }else{
+            if(email.value === validUsuario.email && password.value === validUsuario.password){
+                window.location.href = 'http://127.0.0.1:5500/codigo-fonte/Index.html'
+
+            } else
+            {                
+                labelEmail.setAttribute('style', 'color: red')
+                email.setAttribute('style', 'border-color: red')
+                labelPassword.setAttribute('style', 'color: red')
+                password.setAttribute('style', 'border-color: red')                                   
+                msgErro.setAttribute('style', `display: block`)
+                msgErro.innerHTML = 'Usuário ou senha incorretos' 
+                email.focus();
+            } 
         }
-    
+
+        
+            
     }
